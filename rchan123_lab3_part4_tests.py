3b703d0db22ced8aaca911b3b73ac53076442b23
ryanchangx <ryanchangx@gmail.com>
2021-04-06 22:05:05 -0700

    Added tests for part 4

--- rchan123_lab3_part4_tests.py
+++ rchan123_lab3_part4_tests.py
@@ -18,6 +18,18 @@ tests = [ {'description': 'PINA: 0xFF => PORTB: 0x0F, PORTC: 0xF0',
     'steps': [ {'inputs': [('PINA', 0xFF)], 'iterations': 5 } ],
     'expected': [('PORTB', 0x0F),('PORTC', 0xF0)],
     },
+    {'description': 'PINA: 0x42 => PORTB: 0x04, PORTC: 0x20',
+    'steps': [ {'inputs': [('PINA', 0x42)], 'iterations': 5 } ],
+    'expected': [('PORTB', 0x04),('PORTC', 0x20)],
+    },
+    {'description': 'PINA: 0x00 => PORTB: 0x00, PORTC: 0x00',
+    'steps': [ {'inputs': [('PINA', 0x00)], 'iterations': 5 } ],
+    'expected': [('PORTB', 0x00),('PORTC', 0x00)],
+    },
+    {'description': 'PINA: 0xA5 => PORTB: 0x0A, PORTC: 0x50',
+    'steps': [ {'inputs': [('PINA', 0xA5)], 'iterations': 5 } ],
+    'expected': [('PORTB', 0x0A),('PORTC', 0x50)],
+    },
     ]
 
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
